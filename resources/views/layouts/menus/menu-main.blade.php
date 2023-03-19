135ce4226f95906b96cba02430e898661a761bdc
Dacio <fdacio@gmail.com>
2023-03-19 16:20:49 -0300

    layout

--- resources/views/layouts/menus/menu-main.blade.php
+++ resources/views/layouts/menus/menu-main.blade.php
@@ -1,4 +1,7 @@
 <ul class="navbar-nav flex-column bg-default mt-2 p-2">
+    <li class="nav-item"><a href="{{ route('home') }}" class="nav-link"><i class="fa fa-home"></i> <span
+        class="nav-label">Home</span></a></li>
+
     <li class="nav-item"><a href="{{ route('alunos.index') }}" class="nav-link"><i class="fa fa-users"></i> <span
                 class="nav-label">Alunos</span></a></li>
     <li class="nav-item"><a href="{{ route('home') }}" class="nav-link"><i class="fa fa-list"></i> <span
