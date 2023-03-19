62356f954b5e6e5e6502b9dee6fe35af50718803
Dacio <fdacio@gmail.com>
2023-03-19 16:22:34 -0300

    layout

--- resources/views/layouts/menus/menu-main.blade.php
+++ resources/views/layouts/menus/menu-main.blade.php
@@ -1,7 +1,7 @@
 <ul class="navbar-nav flex-column bg-default mt-2 p-2">
-    <li class="nav-item"><a href="{{ route('home') }}" class="nav-link"><i class="fa fa-home"></i> <span
-        class="nav-label">Home</span></a></li>
 
+    <li class="nav-item"><a href="{{ route('home') }}" class="nav-link"><i class="fa fa-home"></i> <span
+                class="nav-label">Home</span></a></li>
     <li class="nav-item"><a href="{{ route('alunos.index') }}" class="nav-link"><i class="fa fa-users"></i> <span
                 class="nav-label">Alunos</span></a></li>
     <li class="nav-item"><a href="{{ route('home') }}" class="nav-link"><i class="fa fa-list"></i> <span
@@ -9,6 +9,6 @@
     <li class="nav-item"><a href="{{ route('home') }}" class="nav-link"><i class="fa fa-dollar"></i> <span
                 class="nav-label">Mensalidades</span></a></li>
     <li class="nav-item"><a href="{{ route('user.index') }}" class="nav-link"><span
-                class="fa fa-user"></span>Usuários</a></li>
+                class="fa fa-user">Usuários</span></a></li>
 
 </ul>
