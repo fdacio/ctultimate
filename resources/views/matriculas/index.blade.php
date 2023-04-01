4d90e5a6b5beaec747c07ab1a8709b475cb45073
Dacio <fdacio@gmail.com>
2023-03-31 23:29:40 -0300

    crud matriculas

--- resources/views/matriculas/index.blade.php
+++ resources/views/matriculas/index.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
-@section('title', 'Alunos')
+@section('title', 'Matrículas')
 
 @section('content')
 <div class="card mb-2">
     <div class="card-header">
-        <h3>Alunos</h3>
+        <h3>Matrículas</h3>
         @if(session('success'))
         <div class="alert alert-success alert-dismissible">
             <button type="button" class="close" data-dismiss="alert" aria-label="Fechar"><span aria-hidden="true">&times;</span></button>
@@ -13,12 +13,12 @@
         @endif
     </div>
     <div class="card-body">
-        <form action="{{ route('alunos.index') }}" method="get" class="form-filter">
+        <form action="{{ route('matriculas.index') }}" method="get" class="form-filter">
             <div class="row">
                 <div class="form-group col-md-12">
-                    <label for="nome">Nome</label>
+                    <label for="matricula">matricula</label>
                     <div class="input-group">
-                        <input type="text" name="nome" id="nome" class="form-control" value="{{ request('nome') }}" />
+                        <input type="text" name="matricula" id="matricula" class="form-control" value="{{ request('matricula') }}" />
                         <div class="input-group-append">
                             <button class="btn btn-success"><i class="fa fa-search mr-2"></i><span>Pesquisar</span></button>
                         </div>
@@ -29,36 +29,36 @@
     </div>
     <div class="card-footer">
         <div class="text-right mb-2">
-            <a href="{{ route('alunos.create') }}" class="btn btn-primary"><i class="fa fa-plus mr-2"></i>Cadastrar</a>
+            <a href="{{ route('matriculas.create') }}" class="btn btn-primary"><i class="fa fa-plus mr-2"></i>Cadastrar</a>
         </div>
     </div>
 </div>
 <section class="table-responsive">
     <table class="table table-striped table-hover">
         <thead>
-            <th>ID</th>
-            <th>Nome</th>
-            <th>Nascimento</th>
-            <th>Telefone</th>
+            <th>Nº Matrícula</th>
+            <th>Aluno</th>
+            <th>Categoria</th>
+            <th>Data</th>
             <th style="width: 20%;"></th>
         </thead>
         <tbody>
-            @if($alunos->total() == 0)
+            @if($matriculas->total() == 0)
             <tr>
-                <th class="text-center" colspan="5">Nenhum aluno encontrado</th>
+                <th class="text-center" colspan="5">Nenhuma matrícula encontrado</th>
             </tr>
             @else
-            @foreach ($alunos as $aluno)
+            @foreach ($matriculas as $matricula)
             <tr>
-                <td>{{ $aluno->id }}</td>
-                <td class="text-nowrap">{{ $aluno->nome }}</td>
-                <td class="text-nowrap">{{ (isset($aluno->nascimento)) ? \Carbon\Carbon::parse($aluno->nascimento)->format('d/m/Y') : '' }}</td>
-                <td class="text-nowrap">{{ $aluno->telefone }}</td>
+                <td>{{ $matricula->id }}</td>
+                <td class="text-nowrap">{{ $matricula->aluno->nome }}</td>
+                <td class="text-nowrap">{{ $matricula->categoria->nome }}</td>
+                <td class="text-nowrap">{{ \Carbon\Carbon::parse($aluno->nascimento)->format('d/m/Y') }}</td>
                 <td class="text-nowrap text-right">
-                    <a href="{{ route('alunos.show', $aluno->id) }}" class="btn btn-info btn-sm" title="Visualizar"><i class="fa fa-eye"></i></a>
-                    <a href="{{ route('alunos.edit', $aluno->id) }}" class="btn btn-primary btn-sm" title="Editar"><i class="fa fa-pencil"></i></a>
-                    @if($alunos->total() > 0)
-                    {!! Form::open(['id' => 'form_excluir_' . $aluno->id, 'method' => 'delete', 'route' => ['alunos.destroy', $aluno->id], 'style'=>'display: inline']) !!}
+                    <a href="{{ route('matriculas.show', $matricula->id) }}" class="btn btn-info btn-sm" title="Visualizar"><i class="fa fa-eye"></i></a>
+                    <a href="{{ route('matriculas.edit', $matricula->id) }}" class="btn btn-primary btn-sm" title="Editar"><i class="fa fa-pencil"></i></a>
+                    @if($matriculas->total() > 0)
+                    {!! Form::open(['id' => 'form_excluir_' . $matricula->id, 'method' => 'delete', 'route' => ['matriculas.destroy', $matricula->id], 'style'=>'display: inline']) !!}
                     {!! Form::button('<i class="fa fa-trash"></i>', ['class' => 'btn btn-danger btn-sm modal-excluir']) !!}
                     {!! Form::close() !!}
                     @endif
@@ -71,12 +71,12 @@
 </section>
 
 <section class="text-center">
-    {{ $alunos->appends(request()->query())->links() }}
-    <h6><b>{{ $alunos->total() }}</b> {{ $alunos->total() == 1 ? 'registro' : 'registros' }} no total</h6>
+    {{ $matriculas->appends(request()->query())->links() }}
+    <h6><b>{{ $matriculas->total() }}</b> {{ $matriculas->total() == 1 ? 'registro' : 'registros' }} no total</h6>
 </section>
 @endsection
 
-@if($alunos->total() > 0)
+@if($matriculas->total() > 0)
 @section('scripts')
 {!! Html::script('js/modal-excluir.js') !!}
 @endsection
